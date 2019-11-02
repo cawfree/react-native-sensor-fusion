27c3699796871198c8f8ce57589e6cb703754183
cawfree <hello@cawfree.com>
2019-11-02 18:06:41 +0000

    🚀 Published to GitHub!

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # react-native-sensor-fusion
-Precise, robust absolute positioning in React Native.
+Precise, robust absolute 3D positioning in React Native, using [sensor fusion]() to exploit the superior characterstics of the device Accelerometer, Gyroscope and Magnetometer, whilst mitigating against their negative qualities.
+
+Physical data acquisition is implemented using [react-native-sensors](https://github.com/react-native-sensors/). The noise in sampled data is filtered from the input signals using the high quality [kalmanjs](https://github.com/wouterbulten/kalmanjs), and sensor fusion is calculated using [ahrs](https://github.com/psiphi75/ahrs).
+
+## 🚀 Getting Started
+
+Using [`npm`]():
+
+```sh
+npm install --save react-native-sensor-fusion
+```
+
+Using [`yarn`]():
+
+```sh
+yarn add react-native-sensor-fusion
+```
+
+## ✍️ Example
+
+```javascript
+import React from 'react';
+import { Text } from 'react-native';
+import SensorFusionProvider, { useSensorFusion } from 'react-native-sensor-fusion';
+
+const Indicator = () => {
+  const { ahrs } = useSensorFusion();
+  const { heading, pitch, roll } = ahrs.getEulerAngles();
+  return (
+    <Text>
+      Heading: {heading / Math.PI * 180}°{'\n'}
+      Pitch: {pitch / Math.PI * 180}°{'\n'}
+      Roll: {roll / Math.PI * 180}°{'\n'}
+    </Text>
+  );
+};
+
+export default () => (
+  <SensorFusionProvider>
+    <Indicator />
+  </SensorFusionProvider>
+);
+```
+
+## 📌 Prop Types
+
+-----
+Prop                  | Type     | Default                   | Required | Description
+--------------------- | -------- | ------------------------- | -------- | -----------
+sampleInterval|number|60|No|The frequency to sample the device sensors in Hz.
+algorithm|string|'Mahony'|No|Choose from the `Madgwick` or `Mahony` filter.
+beta|number|0.4|No|The filter noise value, smaller values have smoother estimates, but have higher latency. This only works for the `Madgwick` filter.
+kp|number|0.5|No|The filter noise values for the `Mahony` filter (Proportional).
+ki|number|0|No|The filter noise values for the `Mahony` filter (Integral).
+
+## ✌️ License
+[MIT](https://opensource.org/licenses/MIT)
---
+++ index.js
@@ -0,0 +1,126 @@
+import React, { useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import * as Sensors from 'react-native-sensors';
+import Ahrs from 'ahrs';
+import KalmanFilter from 'kalmanjs';
+
+const types = ['gyroscope', 'accelerometer', 'magnetometer']
+  .map(
+    type => Sensors.SensorTypes[type],
+  );
+
+const SensorFusionContext = React
+  .createContext(
+    null,
+  );
+
+const createFilters = () => types
+  .map(
+    () => [...Array(3)].map(
+      () => new KalmanFilter(),
+    ),
+  );
+
+const SensorFusionProvider = ({ children, ...extraProps }) => {
+  const { sampleInterval } = extraProps;
+  const [ ahrs, setAhrs ] = useState(
+    new Ahrs(extraProps),
+  );
+  const [
+    filters,
+    setFilters,
+  ] = useState(
+    createFilters(),
+  );
+  const [ gyro ] = useState([ 0, 0, 0 ]);
+  const [ accl ] = useState([ 0, 0, 0 ]);
+  const [ comp ] = useState([ 0, 0, 0 ]);
+  const get = [ gyro, accl, comp ];
+  const [ value, setValue ] = useState(
+    {
+      ahrs,
+    },
+  );
+  useEffect(
+    () => {
+      const ahrs = new Ahrs(extraProps);
+      const filters = createFilters();
+      setAhrs(ahrs);
+      setValue({ ahrs });
+      setFilters(
+        filters,
+      );
+    },
+    Object.keys(extraProps),
+  );
+  useEffect(
+    () => types
+      .map(
+        type => Sensors.setUpdateIntervalForType(
+          type,
+          (1000 / sampleInterval),
+        ),
+      )
+      .reduce(() => undefined),
+    [ sampleInterval ],
+  );
+  useEffect(
+    () => {
+      const subscriptions = types
+        .map(
+          (type, i) => Sensors[type]
+            .subscribe(
+              ({ x, y, z }) => {
+                get[i][0] = filters[i][0].filter(x);
+                get[i][1] = filters[i][2].filter(y);
+                get[i][2] = filters[i][2].filter(z);
+                ahrs
+                  .update(
+                    ...get[0],
+                    ...get[1],
+                    ...get[2],
+                  );
+                if (i === types.length - 1) {
+                  setValue(
+                    {
+                      ahrs,
+                    },
+                  );
+                }
+              }
+            ),
+        );
+      return () => subscriptions
+        .map(({ unsubscribe }) => unsubscribe());
+    },
+    [],
+  );
+  return (
+    <SensorFusionContext.Provider
+      value={value}
+      children={children}
+    />
+  );
+};
+
+SensorFusionProvider.propTypes = {
+  sampleInterval: PropTypes.number,
+  algorithm: PropTypes.string,
+  beta: PropTypes.number,
+  kp: PropTypes.number,
+  ki: PropTypes.number,
+  doInitialization: PropTypes.bool,
+};
+
+SensorFusionProvider.defaultProps = {
+  sampleInterval: 60,
+  algorithm: 'Mahony',
+  beta: 0.4,
+  kp: 0.5,
+  ki: 0,
+  doInitialization: false,
+};
+
+export const useSensorFusion = () => useContext(SensorFusionContext);
+
+export default SensorFusionProvider;
